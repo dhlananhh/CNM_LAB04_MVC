84c6ef69e55999c1cb9da2972273262aef42a56e
Lan Anh <dhlananh2309@gmail.com>
2025-03-19 23:33:43 +0700

    feat: add new courses and semester selection option in data.js and index.ejs

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+const { name } = require("ejs");
+
 const courses = [
   {
     id: 1,
@@ -23,6 +25,27 @@ const courses = [
   {
     id: 4,
     name: 'Công nghệ mới',
+    course_type: 'Chuyên ngành',
+    semester: 'HK1/2020-2021',
+    department: 'Khoa CNTT',
+  },
+  {
+    id: 5,
+    name: 'Phân tích thiết kế hệ thống',
+    course_type: 'Chuyên ngành',
+    semester: 'HK1/2020-2021',
+    department: 'Khoa CNTT',
+  },
+  {
+    id: 6,
+    name: 'Phát triển ứng dụng web',
+    course_type: 'Chuyên ngành',
+    semester: 'HK1/2020-2021',
+    department: 'Khoa CNTT',
+  },
+  {
+    id: 7,
+    name: 'Lập trình web',
     course_type: 'Chuyên ngành',
     semester: 'HK1/2020-2021',
     department: 'Khoa CNTT',
